f0566dde18db3905d93be97145614d4f35f3ded5
walterjesse <61823856+walterjesse@users.noreply.github.com>
2023-06-03 13:29:35 +0300

    Update settings.py

--- Smazigah1/settings.py
+++ Smazigah1/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='mysecretkey254')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
