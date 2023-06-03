1b20f0bbe122f2b15d011edec735a481a193eaa9
walterjesse <61823856+walterjesse@users.noreply.github.com>
2023-06-03 13:41:06 +0300

    Update settings.py

--- Smazigah1/settings.py
+++ Smazigah1/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='mysecretkey254')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.onrender.com']
 
